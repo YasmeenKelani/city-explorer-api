f2a43e3e3e43fd0976ef63cc6c7cf633c3faf10e
YasmeenKelani <yasminekailani98@gmail.com>
2021-09-29 20:08:46 +0300

    lab10

--- module/movies.js
+++ module/movies.js
@@ -1,40 +1,49 @@
-'use strict';
+"use strict";
 
 const axios = require("axios");
+let cache = require("./cache.js");
+function getMovieFun(req, res) {
+  let searchQuery = req.query.searchQuery;
 
-function getMovieFun (req, res){
+  let reqUrlmovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
 
-    let searchQuery = req.query.searchQuery;
+  if (cache[searchQuery] !== undefined) {
+    console.log("the cashe contain data ");
+    console.log(cache);
+    res.send(cache[searchQuery]);
+  } else {
+    console.log("cache memory is empty hit the api");
 
-    let reqUrlmovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY }&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
-    
     console.log(reqUrlmovie);
-    console.log(req.query)
-     axios.get(reqUrlmovie).then(movieInfo=>{
-    console.log(movieInfo) 
-    console.log(movieInfo.results)                  
-    
-    let Array = movieInfo.data.results.map(info=>
-    {return new Movie(info.title,info.poster_path, info.popularity)
-    })
+    console.log(req.query);
+    axios
+      .get(reqUrlmovie)
+      .then((movieInfo) => {
+        console.log(movieInfo);
+        console.log(movieInfo.results);
 
-res.send(Array)
-  }).catch(error => { res.send(error)
-  });
+        let Arraym= movieInfo.data.results.map((info) => {
+          return new Movie(info.title, info.poster_path, info.popularity);
+        });
+
+        cache[searchQuery] = Arraym;
+        res.send(Arraym);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   }
 
-    // console.log("l", locResult);
-    // console.log("seclocResult", weather.data);
+  // console.log("l", locResult);
+  // console.log("seclocResult", weather.data);
 
-    class Movie {
-    constructor(title, poster_path,popularity){
-    this.title=title
-    this.poster_path= "https://image.tmdb.org/t/p/w500" + poster_path 
+  class Movie {
+    constructor(title, poster_path, popularity) {
+      this.title = title;
+      this.poster_path = "https://image.tmdb.org/t/p/w500" + poster_path;
 
-    this.popularity=popularity
+      this.popularity = popularity;
+    }
+  }
 }
-}
-    
- 
-
-module.exports=getMovieFun;
+module.exports = getMovieFun;
--- module/weather.js
+++ module/weather.js
@@ -1,38 +1,48 @@
-'use strict';
+"use strict";
 
 const axios = require("axios");
+let cache = require("./cache.js");
 
-function getWeatherFun (req, res){
+function getWeatherFun(req, res) {
+  let searchQuery = req.query.city;
+  let reqUrlw = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery},&key=${process.env.WEATHER_API_KEY}`;
 
-    let searchQuery = req.query.searchQuery;
+  if (cache[searchQuery] !== undefined) {
+    console.log("the cashe contain data ");
+    console.log(cache);
+    res.send(cache[searchQuery]);
+  } else {
+    console.log("cache memory is empty hit the api");
 
-    let reqUrlw = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery},&key=${process.env.WEATHER_API_KEY}`;
-    
     console.log(reqUrlw);
-    console.log(req.query)
-     axios.get(reqUrlw).then(weatherinfo=>{
-    console.log(weatherinfo) 
-    console.log(weatherinfo.data)                  
-    
-    let Array = weatherinfo.data.data.map(info=>
-    {return new Forcast(info.weather.description, info.datetime)
-    })
+    console.log(req.query);
+    axios
+      .get(reqUrlw)
+      .then((weatherinfo) => {
+        console.log(weatherinfo);
+        console.log(weatherinfo.data);
 
-res.send(Array)
-  }).catch(error => { res.send(error)
-  });
+        let Array = weatherinfo.data.data.map((info) => {
+          return new Forcast(info.weather.description, info.datetime);
+        });
+
+        cache[searchQuery] = Array;
+        res.send(Array);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   }
 
-    // console.log("l", locResult);
-    // console.log("seclocResult", weather.data);
+  // console.log("l", locResult);
+  // console.log("seclocResult", weather.data);
 
-    class Forcast {
-    constructor(description, date){
-    this.description = description
-    this.date=date
+  class Forcast {
+    constructor(description, date) {
+      this.description = description;
+      this.date = date;
+    }
+  }
 }
-}
-    
- 
 
-module.exports=getWeatherFun;
+module.exports = getWeatherFun;
